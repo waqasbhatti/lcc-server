bcf1cdbe2f2acb7f5e7aa784125afdfddd7bdf43
Waqas Bhatti <wbhatti@astro.princeton.edu>
2018-05-09 13:14:09 -0400

    abcat: added default column descriptions;

--- lccserver/objectsearch/abcat.py
+++ lccserver/objectsearch/abcat.py
@@ -90,6 +90,433 @@ from tqdm import tqdm
 
 from astrobase import lcdb
 
+
+#########################################
+## DEFAULT COLUMNS THAT ARE RECOGNIZED ##
+#########################################
+
+COLUMN_INFO = {
+    'abs_gaiamag':{
+        'title':'M<sub>G</sub> [mag]',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'absolute GAIA magnitude'
+    },
+    '{magcol}.beyond1std':{
+        'title':'f<sub>&gt; 1.0-&sigma;</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':('fraction of measurements beyond 1-stdev '
+                       'for mag col: {magcol}')
+    },
+    '{magcol}.comments':{
+        'title':'comments',
+        'dtype':'U600',
+        'format':'%s',
+        'description':("comments on the object and its time-series "
+                       "for mag col: {magcol}")
+    },
+    '{magcol}.eta_normal':{
+        'title':'&eta;',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':('eta variability index of the '
+                       'time-series for mag col: {magcol}')
+    },
+    '{magcol}.kurtosis':{
+        'title':'kurtosis',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':'kurtosis of the time-series for mag col: {magcol}'
+    },
+    '{magcol}.linear_fit_slope':{
+        'title':'m<sub>linfit</sub>',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':('slope of a linear fit to the time-series '
+                       'for mag col: {magcol}')
+    },
+    '{magcol}.mad':{
+        'title':'MAD',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':('median absolute deviation of the time-series '
+                       'for mag col: {magcol}')
+    },
+    '{magcol}.mag_iqr':{
+        'title':'IQR',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':('interquartile range of the time-series '
+                       'for mag col: {magcol}')
+    },
+    '{magcol}.magnitude_ratio':{
+        'title':'&Delta;',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':('(max mag - med mag)/(max mag - min mag) '
+                       'for mag col: {magcol}')
+    },
+    '{magcol}.median':{
+        'title':'median',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':'median of the time-series for mag col: {magcol}'
+    },
+    '{magcol}.objectisvar':{
+        'title':'variability flag',
+        'dtype':'i8',
+        'format':'%i',
+        'description':("for mag col: {magcol}, 0 = unreviewed, 1 = variable, "
+                       "2 = not variable, 3 = can't tell")
+    },
+    '{magcol}.skew':{
+        'title':'skew',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':'skew of the time-series for mag col: {magcol}'
+    },
+    '{magcol}.stdev':{
+        'title':'&sigma;',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':('standard deviation of the time-series '
+                       'for mag col: {magcol}'),
+    },
+    '{magcol}.stetsonj':{
+        'title':'J<sub>stetson</sub>',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':('Stetson J variability index of the '
+                       'time-series for mag col: {magcol}')
+    },
+    '{magcol}.stetsonk':{
+        'title':'K<sub>stetson</sub>',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':('Stetson K variability index of the '
+                       'time-series for mag col: {magcol}')
+    },
+    '{magcol}.varepoch':{
+        'title':'epoch [JD]',
+        'dtype':'f8',
+        'format':'%.6f',
+        'description':('for mag col: {magcol}, JD epoch of minimum '
+                       ' light if periodic variable')
+    },
+    '{magcol}.varisperiodic':{
+        'title':'periodicity flag',
+        'dtype':'i8',
+        'format':'%i',
+        'description':('for mag col: {magcol}, 0 = undetermined, 1 = periodic, '
+                       '2 = not periodic, 3 = quasi-periodic')
+    },
+    '{magcol}.varperiod':{
+        'title':'period [days]',
+        'dtype':'f8',
+        'format':'%.6f',
+        'description':'for mag col: {magcol}, period of variability in days'
+    },
+    '{magcol}.vartags':{
+        'title':'variable tags',
+        'dtype':'U600',
+        'format':'%s',
+        'description':'for mag col: {magcol}, variability tags for this object'
+    },
+    'bmag':{
+        'title':'<em>B</em>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'catalog magnitude in B filter'
+    },
+    'color_classes':{
+        'title':'color classes',
+        'dtype':'U600',
+        'format':'%s',
+        'description':'stellar classification using SEGUE color cuts in ugriz'
+    },
+    'decl':{
+        'title':'&delta;',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':'declination of the object [J2000 decimal degrees]'
+    },
+    'extinction_bmag':{
+        'title':'A<sub>B</sub> [mag]',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'extinction in B band [mag]'
+    },
+    'extinction_hmag':{
+        'title':'A<sub>H</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'extinction in 2MASS H band [mag]'
+    },
+    'extinction_imag':{
+        'title':'A<sub>I</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'extinction in I band [mag]'
+    },
+    'extinction_jmag':{
+        'title':'A<sub>J</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'extinction in 2MASS J band [mag]'
+    },
+    'extinction_kmag':{
+        'title':'A<sub>Ks</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'extinction in 2MASS Ks band [mag]'
+    },
+    'extinction_rmag':{
+        'title':'A<sub>R</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'extinction in R band [mag]'
+    },
+    'extinction_sdssg':{
+        'title':'A<sub>g</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'extinction in SDSS g band [mag]'
+    },
+    'extinction_sdssi':{
+        'title':'A<sub>i</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'extinction in SDSS i band [mag]'
+    },
+    'extinction_sdssr':{
+        'title':'A<sub>r</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'extinction in SDSS r band [mag]'
+    },
+    'extinction_sdssu':{
+        'title':'A<sub>u</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'extinction in SDSS u band [mag]'
+    },
+    'extinction_sdssz':{
+        'title':'A<sub>z</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'extinction in SDSS z band [mag]'
+    },
+    'extinction_vmag':{
+        'title':'A<sub>V</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'extinction in V band [mag]'
+    },
+    'gaia_id':{
+        'title':'GAIA DR2 ID',
+        'dtype':'U40',
+        'format':'%s',
+        'description':'cross-matched GAIA DR2 source ID of the object'
+    },
+    'gaia_parallax':{
+        'title':'&omega; [mas]',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':'GAIA DR2 parallax of the object in milliarcsec'
+    },
+    'gaia_parallax_err':{
+        'title':'&sigma;<sub>&omega;</sub>',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':('error in the GAIA DR2 parallax of '
+                       'the object in milliarcsec')
+    },
+    'gaia_status':{
+        'title':'GAIA status',
+        'dtype':'U50',
+        'format':'%s',
+        'description':'GAIA cross-match status'
+    },
+    'gaiamag':{
+        'title':'G [mag]',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'GAIA DR2 magnitude'
+    },
+    'gb':{
+        'title':'M<sub>G</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'absolute GAIA magnitude'
+    },
+    'gl':{
+        'title':'l',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':'galactic longitude [decimal degrees]'
+    },
+    'hmag':{
+        'title':'<em>H</em>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'catalog 2MASS H band magnitude'
+    },
+    'imag':{
+        'title':'<em>I</em>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'catalog I band magnitude'
+    },
+    'jmag':{
+        'title':'<em>J</em>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'catalog 2MASS J band magnitude'
+    },
+    'kmag':{
+        'title':'<em>K<sub>s</sub></em>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'catalog 2MASS Ks band magnitude'
+    },
+    'lcfname':{
+        'title':'LC filename',
+        'dtype':'U600',
+        'format':'%s',
+        'description':'light curve filename'
+    },
+    'ndet':{
+        'title':'M<sub>G</sub>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'absolute GAIA magnitude'
+    },
+    '{magcol}.ndet':{
+        'title':'ndet ({magcol})',
+        'dtype':'i8',
+        'format':'%i',
+        'description':('number of non-nan time-series observations '
+                       'for mag col: {magcol}')
+    },
+    'objectid':{
+        'title':'object ID',
+        'dtype':'U600',
+        'format':'%s',
+        'description':'object ID'
+    },
+    'objecttags':{
+        'title':'object tags',
+        'dtype':'U600',
+        'format':'%s',
+        'description':'object type tags'
+    },
+    'pmdecl':{
+        'title':'pmDEC [mas/yr]',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':'proper motion in declination [mas/yr]'
+    },
+    'pmra':{
+        'title':'pmRA [mas/yr]',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':'proper motion in right ascension [mas/yr]'
+    },
+    'propermotion':{
+        'title':'PM [mas/yr]',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':'total proper motion [mas/yr]'
+    },
+    'ra':{
+        'title':'&alpha;',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':'right ascension of the object [J2000 decimal degrees]'
+    },
+    'rmag':{
+        'title':'<em>R</em>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'catalog R band magnitude'
+    },
+    'rpmj':{
+        'title':'RPM<sub>J</sub>',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':'reduced proper motion of the object using 2MASS J mag'
+    },
+    'sdssg':{
+        'title':'<em>g</em>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'catalog SDSS g band magnitude'
+    },
+    'sdssi':{
+        'title':'<em>i</em>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'catalog SDSS i band magnitude'
+    },
+    'sdssr':{
+        'title':'<em>r</em>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'catalog SDSS r band magnitude'
+    },
+    'sdssu':{
+        'title':'<em>u</em>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'catalog SDSS u band magnitude'
+    },
+    'sdssz':{
+        'title':'<em>z</em>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'catalog SDSS z band magnitude'
+    },
+    'simbad_best_mainid':{
+        'title':'external object ID',
+        'dtype':'U50',
+        'format':'%s',
+        'description':'main SIMBAD ID for this object'
+    },
+    'simbad_best_objtype':{
+        'title':'type flag',
+        'dtype':'U20',
+        'format':'%s',
+        'description':('<a href="http://simbad.u-strasbg.fr/guide/chF.htx">'
+                       'SIMBAD object type flag for this object</a>')
+    },
+    'simbad_best_allids':{
+        'title':'other IDs',
+        'dtype':'U600',
+        'format':'%s',
+        'description':'other object IDs from SIMBAD'
+    },
+    'simbad_best_distarcsec':{
+        'title':'SIMBAD dist',
+        'dtype':'f8',
+        'format':'%.5f',
+        'description':'distance in arcseconds from closest SIMBAD cross-match'
+    },
+    'twomassid':{
+        'title':'2MASS ID',
+        'dtype':'U60',
+        'format':'%s',
+        'description':'2MASS ID for this object'
+    },
+    'vmag':{
+        'title':'<em>V</em>',
+        'dtype':'f8',
+        'format':'%.3f',
+        'description':'catalog V band magnitude'
+    },
+}
+
+
 #####################################################
 ## FUNCTIONS TO BREAK OUT INFO FROM LCPROC RESULTS ##
 #####################################################
@@ -127,12 +554,12 @@ def kdtree_from_lclist(lclistpkl, outfile):
         return None
 
 
-
 def objectinfo_to_sqlite(augcatpkl,
                          outfile,
                          colinfo=None,
                          indexcols=None,
                          ftsindexcols=None):
+
     '''This writes the object information to an SQLite file.
 
     makes indexes for fast look up by objectid by default and any columns
@@ -320,8 +747,8 @@ def objectinfo_to_sqlite(augcatpkl,
 
             if col in overridecolinfo:
 
-                if overridecolinfo[col]['name'] is not None:
-                    defaultcolinfo[col]['name'] = overridecolinfo[col]['name']
+                if overridecolinfo[col]['title'] is not None:
+                    defaultcolinfo[col]['title'] = overridecolinfo[col]['name']
 
                 if overridecolinfo[col]['dtype'] is not None:
                     defaultcolinfo[col]['dtype'] = overridecolinfo[col]['dtype']
